537ae887bdd24c04b167d93740e4934db0addf7f
YellowStar5 <codercuixin@gmail.com>
2019-10-16 20:15:50 +0800

    add lecture16

---
+++ src/main/java/lecture16/Test.java
@@ -0,0 +1,14 @@
+package lecture16;
+
+/**
+ * * @Author: cuixin
+ * * @Date: 2019/10/16 19:57
+ */
+public class Test {
+    public static void foo(Object obj){
+        int sum = 0;
+        for(int i=0; i< 200; i++){
+            sum += i;
+        }
+    }
+}
